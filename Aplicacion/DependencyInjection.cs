6f2040685bfdba5ebae6d3ee1190755332db4799
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-04-09 18:01:50 -0600

    DI y Context Impl

---
+++ Aplicacion/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using SistemaHorarios.Aplicacion.Grupos;
+using SistemaHorarios.Aplicacion.Horarios;
+using SistemaHorarios.Aplicacion.Maestros;
+using SistemaHorarios.Aplicacion.MayasCurriculares;
+
+namespace SistemaHorarios.Aplicacion;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient<IGrupoService, GrupoService>();
+        services.AddTransient<IHorarioService, HorarioService>();
+        services.AddTransient<IMaestroService, MaestroService>();
+        services.AddTransient<IMayaCurricularService, MayaCurricularService>();
+
+        return services;
+    }
+}
---
+++ Infrastructura/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SistemaHorarios.Infrastructura;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionString")
+            ?? throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);    
+        });
+
+        return services;
+    }
+}
