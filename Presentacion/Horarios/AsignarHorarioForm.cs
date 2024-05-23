7d509133dce37ad0eaf2ab614e21fb6ec3d0f269
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-05-23 15:20:30 -0600

    Ultima version compilando

--- Presentacion/Horarios/AsignarHorarioForm.cs
+++ Presentacion/Horarios/AsignarHorarioForm.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using ReaLTaiizor.Forms;
+﻿using ReaLTaiizor.Forms;
 
 namespace Presentacion.Horarios
 {
@@ -16,7 +7,6 @@ namespace Presentacion.Horarios
         public AsignarHorarioForm()
         {
             InitializeComponent();
-            parrotCard1
         }
     }
 }
